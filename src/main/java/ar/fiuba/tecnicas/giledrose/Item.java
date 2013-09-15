2149174622e6c059a616876317034743ab42c7c9
Marco Lotto <marcol91@gmail.com>
2013-09-15 17:38:26 -0300

    Giled Rose, funcionando con pruebas unitarias

---
+++ src/main/java/ar/fiuba/tecnicas/giledrose/Item.java
@@ -0,0 +1,40 @@
+package ar.fiuba.tecnicas.giledrose;
+
+
+public class Item {
+    private String name;
+    private int sellIn;
+
+    private int quality;
+
+    public Item(String name, int sellIn, int quality) {
+        super();
+        this.name = name;
+        this.sellIn = sellIn;
+        this.quality = quality;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getSellIn() {
+        return sellIn;
+    }
+
+    public void setSellIn(int sellIn) {
+        this.sellIn = sellIn;
+    }
+
+    public int getQuality() {
+        return quality;
+    }
+
+    public void setQuality(int quality) {
+        this.quality = quality;
+    }
+}
---
+++ src/test/java/ar/fiuba/tecnicas/giledrose/GiledRoseTest.java
@@ -0,0 +1,13 @@
+package ar.fiuba.tecnicas.giledrose;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+
+public class GiledRoseTest {
+    @Test
+    public void replaceWithRealTests() {
+        assertEquals("replace with real tests!", false, true);
+    }
+}
